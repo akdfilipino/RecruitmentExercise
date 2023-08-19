fff6546d0286f12ebda49074939c85d332796fce
InfinixPryer <akdfilipino@gmail.com>
2023-08-20 05:34:09 +0800

    Refactor FE code for Deployment - Add web.config

--- client/src/components/TerritoryListComponent.vue
+++ client/src/components/TerritoryListComponent.vue
@@ -8,7 +8,7 @@
     <div :key="1" class="contents">
             <h1>Territories</h1>
             <p>Here are the list of territories.</p>
-            <ul id="myUI" v-for="territory in this.territories">
+            <ul id="myUI" v-for="territory in territories">
                 <TerritoryComponent :key="territory.id" :territory="territory"/>
             </ul>
         </div>
